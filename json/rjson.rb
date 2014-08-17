cefb789c81549825efd594b38bdf14eb7e250217
Benjamin Fleischer <github@benjaminfleischer.com>
2014-08-17 12:04:16 -0500

    Clean up API

--- json/rjson.rb
+++ json/rjson.rb
@@ -1,8 +1,13 @@
 # @example
 # require 'rjson'
 #
-# input   = StringIO.new '{"foo":"bar"}'
-# tok     = RJSON::Tokenizer.new input
-# parser  = RJSON::Parser.new tok
-# handler = parser.parse
-# handler.result # => {"foo"=>"bar"}
+# RJSON.load(input) # => {"foo"=>"bar"}
+module RJSON
+  def self.load(json)
+    input   = StringIO.new json
+    tok     = RJSON::Tokenizer.new input
+    parser  = RJSON::Parser.new tok
+    handler = parser.parse
+    handler.result
+  end
+end
