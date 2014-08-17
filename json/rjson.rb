76ad95cd6f163521642ce48c61103ea88e979616
Benjamin Fleischer <github@benjaminfleischer.com>
2014-08-17 12:06:24 -0500

    Allow any IO

--- json/rjson.rb
+++ json/rjson.rb
@@ -1,13 +1,17 @@
 # @example
 # require 'rjson'
-#
-# RJSON.load(input) # => {"foo"=>"bar"}
+# require 'open-uri'
+# RJSON.load '{"foo":"bar"}' # => {"foo"=>"bar"}
+# RJSON.load_io open('http://example.org/some_endpoint.json')
 module RJSON
-  def self.load(json)
-    input   = StringIO.new json
+  # allow passing a socket or file handle
+  def self.load_io(input)
     tok     = RJSON::Tokenizer.new input
     parser  = RJSON::Parser.new tok
     handler = parser.parse
     handler.result
+  end
+  def self.load(json)
+    load_io StringIO.new json
   end
 end
